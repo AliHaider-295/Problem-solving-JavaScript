3122ba0b677a3411ae1e3d4d5c5a04b7b6c33607
ali haider <aleehaider953@gmail.com>
2026-04-03 23:22:56 +0500

    swap two numbers with out using third variables

---
+++ Day-1/swap.js
@@ -0,0 +1,29 @@
+// swap two numbers a and b  without useing thrid variable 
+
+
+let a = 5;
+let b = 6;
+
+console.log( `values before swapping a=${a} and b=${b}`)
+
+
+// a = b;
+// b = a;
+
+console.log( `values before swapping a=${a} and b=${b}`)
+
+
+
+// let c = b 
+//   b = a ;
+//   a = c;
+
+
+// with out using thrid vairable now 
+
+
+ a = a + b;
+
+ b = a - b;
+ a = a - b
+console.log( `values before swapping a=${a} and b=${b}`)
