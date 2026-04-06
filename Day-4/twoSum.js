f8d51f767ba53b451aef90caf24dddde26ff269c
ali haider <aleehaider953@gmail.com>
2026-04-06 23:00:19 +0500

    updated

---
+++ Day-4/twoSum.js
@@ -0,0 +1,16 @@
+// function twoSum ( nums, targets){
+
+//     for(let i = 0; i < nums.length; i ++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             if( nums[i] + nums[j] === targets){
+//                 return [i , j]
+//             }
+//         }
+//     }
+// }
+
+
+
+function reversed(str){
+    for(let i = 0; str.length-1 ; i -- )
+}
